a5737df4b495fe98cd560d5d031dc2a69c9a05d9
Sabbir <sabbir.ahmad111@gmail.com>
2017-11-23 15:07:34 +0600

    Admit show page added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,17 +6,35 @@
 </head>
 <body>
 
-	<div class="col-md-4 col-md-offset-4" style="margin-top:100px;border-style: solid;
-    border-color: black;">
-			<div class="col-md-12" style="margin-top: 20px;">
-				<label for="male">Name : </label><br>
-				<label for="male">Father Name : </label><br>
-				<label for="male">Roll Number : </label><br>
-			
-				<label for="male">Profession : </label><br>
-				<label for="male">Khamis Passed year : </label><br>
-				
-		    </div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2" style="margin-top:100px;">
+				<div class="col-md-12">
+					<div class="col-md-2">
+						<img src="http://visitourpage.com/wp-content/uploads/2014/09/250x250.png" style="height:50px;width:50px;">
+					</div>
+					<div class="col-md-10">
+						<h3>Darul Kirat Mojidiya Fultoli Trust,Fultoli Shaheb Bari,Jokigonj, Sylhet</h3>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="col-md-10 col-md-offset-1">
+						<h4 class="text-center">Admit Card</h4>
+						<img src="http://visitourpage.com/wp-content/uploads/2014/09/250x250.png" class="pull-right" style="height:50px;width:50px;">
+					</div>
+					<div class="col-md-10 col-md-offset-1" style="margin-top:30px;">
+						<p class="pull-left">Year : 2018</p>
+						<p class="pull-right">Jamat : Chadis</p>
+						<br>
+						<br>
+					</div>
+					<div class="col-md-10 col-md-offset-1">
+						<p>Name : </p>
+						<p>Father Name:</p>
+						<p>Roll :</p> <p>Porik :</p>
+						<p>Student Type :</p>
+					</div>
+				</div>
+		</div>
 	</div>
 
 </body>
